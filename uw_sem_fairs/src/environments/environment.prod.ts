f159ceeefc1c42b72e45ce441df235a1c83995b1
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-02-04 13:28:12 -0500

    updates for production code

--- uw_sem_fairs/src/environments/environment.prod.ts
+++ uw_sem_fairs/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const ip = '10.0.1.4';
+const baseUrl = `http://${ip}:3000`;
+const fairsUrl = `http://${ip}:4000`;
+
 export const environment = {
-  production: true
+  production: true,
+  url: baseUrl,
+  fairsUrl: fairsUrl
 };
